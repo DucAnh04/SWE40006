db9b343f18b630274e61971f374e2a327282f9dd
ducanh <ducanh15022004@gmail.com>
2025-01-18 21:04:07 +0700

    finsh-project

---
+++ Assigment_1/ExampleDesktop/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using mcMath;
+
+namespace ExampleDesktop
+{
+    public partial class Form1 : Form
+    {
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button calculateButton;
+        private Label resultLabel;
+        private Label plusLabel;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCustomComponents();
+        }
+
+        private void InitializeCustomComponents()
+        {
+            // TextBox for first number
+            textBox1 = new TextBox();
+            textBox1.Location = new System.Drawing.Point(50, 30);
+            this.Controls.Add(textBox1);
+
+            // Label for plus symbol
+            plusLabel = new Label();
+            plusLabel.Text = "+";
+            plusLabel.Location = new System.Drawing.Point(40, 55);
+            plusLabel.AutoSize = true;
+            this.Controls.Add(plusLabel);
+
+            // TextBox for second number
+            textBox2 = new TextBox();
+            textBox2.Location = new System.Drawing.Point(50, 70);
+            this.Controls.Add(textBox2);
+
+            // Button to perform calculation
+            calculateButton = new Button();
+            calculateButton.Text = "Calculate";
+            calculateButton.Location = new System.Drawing.Point(50, 110);
+            calculateButton.Click += new EventHandler(CalculateButton_Click);
+            this.Controls.Add(calculateButton);
+
+            // Label to display result
+            resultLabel = new Label();
+            resultLabel.Location = new System.Drawing.Point(50, 150);
+            resultLabel.AutoSize = true;
+            this.Controls.Add(resultLabel);
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            if (long.TryParse(textBox1.Text, out long num1) && long.TryParse(textBox2.Text, out long num2))
+            {
+                mcMathComp cls = new mcMathComp();
+                long result = cls.Add(num1, num2);
+                cls.Extra = false;
+                resultLabel.Text = $"Result: {result}";
+            }
+            else
+            {
+                resultLabel.Text = "Please enter valid numbers.";
+            }
+        }
+    }
+}
---
+++ Assigment_1/mcMath/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mcMath
+{
+    /// <summary>   
+    /// Summary description for Class1.   
+    /// </summary>   
+    public class mcMathComp
+    {
+        private bool bTest = false;
+        public mcMathComp()
+        {
+            // TODO: Add constructor logic here   
+        }
+        /// <summary>   
+        /// //This is a test method   
+        /// </summary>   
+        public void mcTestMethod()
+        { }
+        public long Add(long val1, long val2)
+        {
+            return val1 + val2;
+        }
+        /// <summary>   
+        /// //This is a test property   
+        /// </summary>   
+        public bool Extra
+        {
+            get
+            {
+                return bTest;
+            }
+            set
+            {
+                bTest = Extra;
+            }
+        }
+    }
+}
